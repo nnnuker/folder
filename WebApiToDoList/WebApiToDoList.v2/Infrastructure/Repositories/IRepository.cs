46f01895cc521e07a1659a2aedd84072f851b3e8
Valery Larkovich <nnoookk@gmail.com>
2016-09-14 02:11:25 +0300

    basic repo

---
+++ WebApiToDoList/WebApiToDoList.v2/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebApiToDoList.Models;
+
+namespace WebApiToDoList.Infrastructure.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T model);
+        void Delete(int id);
+        void Update(T model);
+        IList<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ WebApiToDoList/WebApiToDoList.v2/Infrastructure/Repositories/LocalRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using WebApiToDoList.Models;
+
+namespace WebApiToDoList.Infrastructure.Repositories
+{
+    public class LocalRepository : IRepository<ToDoItemViewModel>
+    {
+        private static readonly IRepository<ToDoItemViewModel> repository = new LocalRepository();
+        private static List<ToDoItemViewModel> items = new List<ToDoItemViewModel>();
+        private readonly ReaderWriterLockSlim lockSlim = new ReaderWriterLockSlim();
+
+        static LocalRepository()
+        {
+        }
+
+        public static IRepository<ToDoItemViewModel> GetRepository => repository;
+
+        private LocalRepository()
+        {
+        }
+
+        public void Add(ToDoItemViewModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            items.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            items.Remove(items.Find(item => item.ToDoId == id));
+        }
+
+        public void Update(ToDoItemViewModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            lockSlim.EnterWriteLock();
+            try
+            {
+                var item = items.Find(i => i.ToDoId == model.ToDoId);
+                items.Remove(item);
+                items.Add(model);
+            }
+            finally
+            {
+                lockSlim.ExitWriteLock();
+            }
+            
+        }
+
+        public IList<ToDoItemViewModel> GetAll()
+        {
+            lockSlim.EnterReadLock();
+            try
+            {
+                return items;
+            }
+            finally
+            {
+                lockSlim.ExitReadLock();
+            }
+        }
+
+        public ToDoItemViewModel GetById(int id)
+        {
+            lockSlim.EnterReadLock();
+            try
+            {
+                return items.Find(i=>i.ToDoId == id);
+            }
+            finally
+            {
+                lockSlim.ExitReadLock();
+            }
+        }
+    }
+
+}
