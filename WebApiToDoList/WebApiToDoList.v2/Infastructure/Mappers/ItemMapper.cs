0e310bdff96988ee6d7bad67b1f21f167d14bc1e
Natalia Pakki <natalie.pakki@gmail.com>
2016-09-14 02:38:27 +0300

    add mappers:  ToItem() and ToViewModel().

---
+++ WebApiToDoList/WebApiToDoList.v2/Infastructure/Mappers/ItemMapper.cs
@@ -0,0 +1,28 @@
+﻿
+using WebApiToDoList.Infastructure.Repository.DTO;
+using WebApiToDoList.Models;
+
+namespace WebApiToDoList.Infastructure.Mappers {
+    public static class ItemMapper {
+        public static Item ToItem(this ToDoItemViewModel model) {
+            if (model == null) return null;
+            return new Item {
+                Id = model.ToDoId,
+                Name = model.Name,
+                UserId = model.UserId,
+                IsCompleted = model.IsCompleted,
+                RemoteId = null
+            };
+        }
+        public static ToDoItemViewModel ToViewModel(this Item item) {
+            if(item == null)
+                return null;
+            return new ToDoItemViewModel {
+                ToDoId = item.Id,
+                Name = item.Name,
+                UserId = item.UserId,
+                IsCompleted = item.IsCompleted
+            };
+        }
+    }
+}
