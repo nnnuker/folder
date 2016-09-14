fe351837ef914c39f626244b8259101b11a97082
Natalia Pakki <natalie.pakki@gmail.com>
2016-09-14 05:39:33 +0300

    add Worker

--- WebApiToDoList/WebApiToDoList.v2/Global.asax.cs
+++ WebApiToDoList/WebApiToDoList.v2/Global.asax.cs
@@ -1,13 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 using WebApiToDoList.v2.Infastructure.Worker;
     
 namespace WebApiToDoList.v2 {
     public class WebApiApplication : System.Web.HttpApplication {
         //TODO: think about this
-        protected async Task Application_Start() {
+        protected void Application_Start() {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            await Worker.Run();
+            var th = new Thread(new Worker().Run);
+            th.Start();
         }
     }
 }
